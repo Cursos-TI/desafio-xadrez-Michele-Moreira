cda66b7d2575685c81c536ca710294cfce478a1b
Michele Moreira <mikaut.silva@gmail.com>
2025-09-18 21:21:01 +0000

    Refatora a lógica de movimentação das peças no xadrez para melhorar a legibilidade e a estrutura do código

--- xadrez.c
+++ xadrez.c
@@ -3,23 +3,44 @@
 int main() {
 
     //Mover a Torre 5 casas para a direita (For)
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); // Imprime a direção
-    }
+
+        for (int torre = 0; torre < 5; torre++) {
+            printf("Direita\n"); // Imprime a direção
+            printf("\n"); // Linha em branco para separar os movimentos
+        }
 
     // Mover o Bispo 5 casas na diagonal para cima e à direita (while)
-    int b = 0;
-    while (b < 5) {
-        printf("Cima, Direita\n");
-        b++;
-    }
+
+        int bispo = 0;
+        while (bispo < 5) {
+            printf("Cima, Direita\n");
+            bispo++;
+            printf("\n");
+        }
 
     // Mover a Rainha 8 casas para a esquerda (do-while)
-    int r = 0;
-    do {
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
 
+
+        int rainha = 0;
+        do {
+            printf("Esquerda\n");
+            rainha++;
+            printf("\n");
+        } while (rainha < 8);
+
+    //Mover o cavalo duas casas para baixo e uma casa para a esquerda ( for e while)
+    int movimentos_baixo = 2;
+    int movimentos_esquerda = 1;
+
+
+        for (int i = 0; i < movimentos_baixo; i++) {
+            printf("Baixo,\n");
+        }
+
+        int j = 0;
+        while (j < movimentos_esquerda) {
+            printf("Esquerda\n");
+            j++;
+        }
     return 0;
 }
